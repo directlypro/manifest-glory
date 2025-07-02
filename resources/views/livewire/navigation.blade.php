5f7cba9bad89670f77aea2b62e40cf2c4cc9b18d
Providence <111455427+directlypro@users.noreply.github.com>
2025-07-02 23:28:50 +0200

    mgg - updated nav bar

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <nav class="bg-gray-800">
-        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+    <nav class="bg-gray-700">
+        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 p-3">
             <div class="relative flex items-center justify-between h-16">
                 <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                     <!-- Mobile menu button-->
@@ -9,13 +9,14 @@
                     <div class="flex-shrink-0">
                         <a href="home') }}" class="text-white text-lg font-bold">Manifest Glory</a>
                     </div>
+                </div>
+                <div class="flex-1/4">
                     <div class="hidden sm:block sm:ml-6">
                         <div class="flex space-x-4">
                             <a href="NOT" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
                             <a href="about') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
                             <a href="contact') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Giving</a>
                             <a href="login') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Serve</a>
-                            <a href="register') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact Us</a>
                         </div>
                     </div>
                 </div>
@@ -23,10 +24,11 @@
                     <!-- Profile dropdown -->
                     <div class="relative">
                         <div>
-                            <button class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm text-white focus:outline-none focus:shadow-outline-blue transition ease-in-out duration-150">
+                          <a href="register') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact Us</a>
+                            <!-- <button class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm text-white focus:outline-none focus:shadow-outline-blue transition ease-in-out duration-150">
                                 <span class="sr-only">Open user menu</span>
                                 <img class="h-8 w-8 rounded-full" src="asset('images/profile.jpg') }}" alt="Profile">
-                            </button>
+                            </button> -->
                         </div>
                     </div>
                 </div>
