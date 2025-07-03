b13ce8dce43b76ef7f24c96371e472f24b2fb02c
Providence <111455427+directlypro@users.noreply.github.com>
2025-07-03 20:36:21 +0200

    mgg - still working on the nav bar, trust me I'm cooking here

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,23 +1,24 @@
 <div>
     <nav class="bg-gray-700">
 
-        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 p-5">
+        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 p-10">
             <div class="relative flex items-center justify-between h-16">
                 <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                     <!-- Mobile menu button-->
                 </div>
                 <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                    <div class="flex-shrink-0">
-                        <a href="#" class="text-white text-lg font-bold">Manifest Glory</a>
-                    </div>
+                    <a href="/" class=" flex items-center space-x-2">
+                        <img src="{{ asset('/images/mgg_crop.png')}}" alt="Logo" class="w-27 px-1">
+                        {{-- <span class="text-white text-xl font-bold hidden md:inline">Manifest Glory Global</span> --}}
+                    </a>
                 </div>
                 <div class="flex-1/4">
                     <div class="hidden sm:block sm:ml-6">
-                        <div class="flex space-x-4">
-                            <a href="#" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-md font-medium">Home</a>
-                            <a href="#" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-md font-medium">About</a>
-                            <a href="#" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-md font-medium">Giving</a>
-                            <a href="#" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-md font-medium">Serve</a>
+                        <div class="flex space-x-15">
+                            <a href="#" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-xl font-medium">About</a>
+                            <a href="#" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-xl font-medium">Giving</a>
+                            <a href="#" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-xl font-medium">Events</a>
+                            <a href="#" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-xl font-medium">Contact Us</a>
                         </div>
                     </div>
                 </div>
@@ -25,7 +26,7 @@
                     <!-- Profile dropdown -->
                     <div class="relative">
                         <div>
-                          <a href="#" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-md font-medium">Contact Us</a>
+                          <a href="#" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-xl font-medium">Contact Us</a>
                         </div>
                     </div>
                 </div>
