c3f09f04b69c2aded05d5309377545b4b0d8deb5
Providence <111455427+directlypro@users.noreply.github.com>
2025-07-03 19:30:17 +0200

    mgg - still working on the nav bar, trust me I'm cooking here

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,22 +1,23 @@
 <div>
     <nav class="bg-gray-700">
-        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 p-3">
+
+        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 p-5">
             <div class="relative flex items-center justify-between h-16">
                 <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                     <!-- Mobile menu button-->
                 </div>
                 <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                     <div class="flex-shrink-0">
-                        <a href="home') }}" class="text-white text-lg font-bold">Manifest Glory</a>
+                        <a href="#" class="text-white text-lg font-bold">Manifest Glory</a>
                     </div>
                 </div>
                 <div class="flex-1/4">
                     <div class="hidden sm:block sm:ml-6">
                         <div class="flex space-x-4">
-                            <a href="NOT" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-md font-medium">Home</a>
-                            <a href="about') }}" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-md font-medium">About</a>
-                            <a href="contact') }}" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-md font-medium">Giving</a>
-                            <a href="login') }}" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-md font-medium">Serve</a>
+                            <a href="#" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-md font-medium">Home</a>
+                            <a href="#" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-md font-medium">About</a>
+                            <a href="#" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-md font-medium">Giving</a>
+                            <a href="#" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-md font-medium">Serve</a>
                         </div>
                     </div>
                 </div>
@@ -24,23 +25,21 @@
                     <!-- Profile dropdown -->
                     <div class="relative">
                         <div>
-                          <a href="register') }}" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-md font-medium">Contact Us</a>
-                            <!-- <button class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm text-white focus:outline-none focus:shadow-outline-blue transition ease-in-out duration-150">
-                                <span class="sr-only">Open user menu</span>
-                                <img class="h-8 w-8 rounded-full" src="asset('images/profile.jpg') }}" alt="Profile">
-                            </button> -->
+                          <a href="#" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-md font-medium">Contact Us</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
+        <!-- Mobile menu, show/hide based on menu state -->
         <div class="sm:hidden">
             <div class="px-2 pt-2 pb-3 space-y-1">
-                <a href="NOT" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Home</a>
-                <a href="about') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
-                <a href="contact') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Giving</a>
-                <a href="login') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Serve</a>
-                <a href="register') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact Us</a>
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Home</a>
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Giving</a>
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Serve</a>
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact Us</a>
             </div>
         </div>
     </nav>
