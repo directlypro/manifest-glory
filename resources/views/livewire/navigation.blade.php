cb699c5fcb64f9ac67e23394fd5d9bbeba28d326
Providence <111455427+directlypro@users.noreply.github.com>
2025-07-03 21:01:17 +0200

    mgg - still working on the nav bar, trust me I'm cooking here

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -2,14 +2,14 @@
     <nav class="bg-gray-700">
 
         <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 p-10">
-            <div class="relative flex items-center justify-between h-16">
+            <div class="relative flex items-center justify-between h-19">
                 <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                     <!-- Mobile menu button-->
                 </div>
                 <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                     <a href="/" class=" flex items-center space-x-2">
                         <img src="{{ asset('/images/mgg_crop.png')}}" alt="Logo" class="w-27 px-1">
-                        {{-- <span class="text-white text-xl font-bold hidden md:inline">Manifest Glory Global</span> --}}
+                        <span class="text-white text-2xl font-bold hidden md:inline">Manifest Glory Global</span>
                     </a>
                 </div>
                 <div class="flex-1/4">
@@ -26,7 +26,7 @@
                     <!-- Profile dropdown -->
                     <div class="relative">
                         <div>
-                          <a href="#" class="text-gray-300 hover:bg-gray-800 hover:text-white px-3 py-2 rounded-md text-xl font-medium">Contact Us</a>
+                          <button class="bg-amber-300 text-white hover:bg-amber-500 hover:text-gray-100 px-7 py-2 rounded-lg text-2xl font-medium">Give</button>
                         </div>
                     </div>
                 </div>
