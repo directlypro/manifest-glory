33002694acae7d0671a709f7773114bd7bbdff09
Providence <111455427+directlypro@users.noreply.github.com>
2025-07-04 22:22:57 +0200

    mgg - home page initia let up

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,3 +1,11 @@
 <div>
-    hello world
+   <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 p-10">
+        <div class="flex items-center justify-center">
+            <div class="text-center">
+                <h1 class="text-4xl font-bold text-gray-800 mb-4">Welcome to Manifest Glory Global</h1>
+                <p class="text-lg text-gray-600 mb-6">Empowering Lives, Transforming Communities</p>
+                <a href="#" class="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition duration-300">Get Started</a>
+            </div>
+        </div>
+   </div>
 </div>
