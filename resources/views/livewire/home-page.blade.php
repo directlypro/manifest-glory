07b6a258ea79fef35684044c013c34cd4546d35c
Providence <111455427+directlypro@users.noreply.github.com>
2025-07-05 22:55:52 +0200

    mgg - updating the home bar

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,5 +1,13 @@
 <div>
-    <div >
+    <div class="static w-auto">
         <img src="{{ asset('/images/worship_image.jpg')}}" alt="Logo" class="w-auto">
+        <div class="absolute inset-0 bg-red-500 opacity-50">
+            <div class="flex items-center justify-center h-screen">
+                <div class="text-center text-white">
+                    <h1 class="text-4xl font-bold mb-4">Manifest Glory Global</h1>
+                    <p class="text-xl mb-8">Join us in worship and fellowship</p>
+                    <a href="#" class="bg-amber-300 text-white hover:bg-amber-500 hover:text-gray-100 px-7 py-2 rounded-2xl text-xl font-medium">Join Now</a>
+                </div>
+        </div>
     </div>
 </div>
