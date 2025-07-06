b61fe76e508a9a6e9da8ba894f219a0428fe4d2c
Providence <111455427+directlypro@users.noreply.github.com>
2025-07-06 22:03:27 +0200

    mgg - home page update

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -2,6 +2,15 @@
     <div class="flex justify-center static w-auto">
         <img src="{{ asset('/images/worship_image.jpg')}}" alt="Logo" class="w-auto">
 
+            <picture>
+                {{-- <!-- Desktop image -->
+                <source media="(min-width: 1024px)" srcset="{{ asset('/images/worship_image-desktop.jpg') }}">
+                <!-- Tablet image -->
+                <source media="(min-width: 640px)" srcset="{{ asset('/images/worship_image-tablet.jpg') }}">
+                <!-- Mobile image -->
+                <img src="{{ asset('/images/worship_image-mobile.jpg') }}" alt="Worship Image" class="w-auto"> --}}
+            </picture>
+
         <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 p-10 absolute bg-red-500 opacity-50">
             <div class="flex items-center justify-center h-screen">
                 <div class="text-center text-white">
