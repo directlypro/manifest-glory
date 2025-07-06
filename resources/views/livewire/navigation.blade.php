abc1c308423ac6f805fa77a8431337657092c579
Providence <111455427+directlypro@users.noreply.github.com>
2025-07-06 22:02:00 +0200

    mgg - home page update

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,7 +1,7 @@
 <div>
     <nav class="bg-gray-700">
 
-        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 p-10">
+        <div class="max-w-7xl mx-auto px-1 sm:px-6 lg:px-8 p-10">
             <div class="relative flex items-center justify-between h-19">
                 <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                     <!-- Mobile menu button-->
