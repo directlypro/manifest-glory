98a23365f8424df3a6035ea33063cb93208649d3
Providence <111455427+directlypro@users.noreply.github.com>
2025-07-18 18:59:30 +0200

    mgg - added banking details section

---
+++ app/Livewire/BankingDetails.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class BankingDetails extends Component
+{
+    public function render()
+    {
+        return view('livewire.banking-details');
+    }
+}
