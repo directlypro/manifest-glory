ff81958ed72657a4da97422f354b539730fd0688
Providence <111455427+directlypro@users.noreply.github.com>
2025-07-24 22:20:41 +0200

    mgg - added a home page

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Home - Manifest Glory Global')
+
+@section('content')
+    <livewire:home-page />
+    <livewire:about-section />
+    <livewire:giving-section />
+    <livewire:banking-details />
+    <livewire:events-section />
+@endsection
