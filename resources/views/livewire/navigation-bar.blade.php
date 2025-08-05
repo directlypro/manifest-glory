390085a8373053cf739c7289b14ccdbfff56cf86
Providence <111455427+directlypro@users.noreply.github.com>
2025-08-05 21:51:40 +0200

    mgg - updated the smaller view nav bars

--- resources/views/livewire/navigation-bar.blade.php
+++ resources/views/livewire/navigation-bar.blade.php
@@ -88,14 +88,14 @@
             <a href="#about" class="text-gray-700 hover:bg-gradient-to-r hover:from-amber-500 hover:to-red-500 hover:text-white block px-4 py-3 rounded-xl text-lg font-medium transition-all duration-300">
                 About
             </a>
-            <a href="#giving" class="text-gray-700 hover:bg-gradient-to-r hover:from-amber-500 hover:to-red-500 hover:text-white block px-4 py-3 rounded-xl text-lg font-medium transition-all duration-300">
-                Giving
+            <a href="#ministries" class="text-gray-700 hover:bg-gradient-to-r hover:from-amber-500 hover:to-red-500 hover:text-white block px-4 py-3 rounded-xl text-lg font-medium transition-all duration-300">
+                Ministries
+            </a>
+            <a href="#campus" class="text-gray-700 hover:bg-gradient-to-r hover:from-amber-500 hover:to-red-500 hover:text-white block px-4 py-3 rounded-xl text-lg font-medium transition-all duration-300">
+                Campus
             </a>
             <a href="#events" class="text-gray-700 hover:bg-gradient-to-r hover:from-amber-500 hover:to-red-500 hover:text-white block px-4 py-3 rounded-xl text-lg font-medium transition-all duration-300">
                 Events
-            </a>
-            <a href="#contact_us" class="text-gray-700 hover:bg-gradient-to-r hover:from-amber-500 hover:to-red-500 hover:text-white block px-4 py-3 rounded-xl text-lg font-medium transition-all duration-300">
-                Contact Us
             </a>
             <div class="flex space-x-4 pt-4 px-4">
                 <a href="https://facebook.com" target="_blank" aria-label="Facebook" class="text-gray-600 hover:text-white p-2 rounded-full hover:bg-gradient-to-r hover:from-amber-500 hover:to-red-500 transition-all duration-300">
