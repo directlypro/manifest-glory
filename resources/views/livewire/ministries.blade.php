d1df50a5aca7615ddb3cf8af0af55ea0d2a7db68
Providence <111455427+directlypro@users.noreply.github.com>
2025-08-07 22:56:13 +0200

    modifiying the nav bar with ministries section

--- resources/views/livewire/ministries.blade.php
+++ resources/views/livewire/ministries.blade.php
@@ -1,4 +1,5 @@
 <div>
-    Hello world!
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+    This section will have the ministries content.
+    <h1 class="text-3xl font-bold mb-4">Our Ministries</h1>
+    <p class="mb-6">Explore the various ministries that make up Manifest Glory Global. Each ministry plays a vital role in our mission and community.</p>
 </div>
