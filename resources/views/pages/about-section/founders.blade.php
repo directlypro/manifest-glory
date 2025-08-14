7c01347fb9973bc5ab169d7a011116d2fb904c81
Providence <111455427+directlypro@users.noreply.github.com>
2025-08-14 17:56:53 +0200

    mgg - updated the simple stuff on the site

---
+++ resources/views/pages/about-section/founders.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Founders - Manifest Glory Global')
+
+@section('content')
+    <livewire:founders />
+    <!-- Add other about-specific components here -->
+@endsection
---
+++ resources/views/pages/about-section/leadership.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Leadership - Manifest Glory Global')
+
+@section('content')
+    <livewire:leadership />
+    <!-- Add other about-specific components here -->
+@endsection
