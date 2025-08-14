0ea203bebf2cbe9dbb03309f75276e62f332e03c
Providence <111455427+directlypro@users.noreply.github.com>
2025-08-14 18:10:24 +0200

    mgg - updated the founders and leaders page

--- resources/views/livewire/founders.blade.php
+++
@@ -1,3 +0,0 @@
-<div>
-Hello, welcome to the Founders page of Manifest Glory Global. Here, we celebrate the visionaries who laid the foundation for our community and continue to inspire us with their leadership and dedication.
-</div>
