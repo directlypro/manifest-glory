3a5e7591ea60f9c60943a20087ac1dbfebb2abb0
Providence <111455427+directlypro@users.noreply.github.com>
2025-09-01 21:45:25 +0200

    mgg - push

--- resources/views/livewire/events.blade.php
+++ resources/views/livewire/events.blade.php
@@ -183,7 +183,6 @@
                         class="w-full h-full object-cover">
                     <div class="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
                     <p class="absolute bottom-4 left-4 text-white font-medium opacity-0 group-hover:opacity-100 transition-opacity duration-300">Baptism</p>
-
                 </div>
 
                 {{-- <!-- Gallery Image 4 -->
